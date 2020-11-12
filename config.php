87310f1bafc4aab4414ba79bd7fbcf30886a6124
pbaker6 <p.baker@middleton.school.nz>
2020-11-13 10:16:21 +1300

    Create config.php
    Created config file and added it to index

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors', 'on'); // change to 'off' when live
+
+define ("DB_NAME", "00_game_apps");
+define ("DB_USERNAME", "root");
+define ("DB_PASSWORD", "");
+define ("DB_HOST", "localhost");
+
+?>
