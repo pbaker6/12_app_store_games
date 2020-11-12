48a700a3937504cc5071e64b40284c9b2926261b
pbaker6 <p.baker@middleton.school.nz>
2020-11-13 10:31:02 +1300

    Php includes
    Created to and bottom bits..

--- index.php
+++ index.php
@@ -1,39 +1,4 @@
-<!DOCTYPE HTML>
-
-<html lang="en">
-    
-
-<head>
-    <meta charset="utf-8">
-    <meta name="description" content="games, apps, ">
-    <meta name="GTT" content="Game / App Database">
-    <meta name="keywords" content="games, apps, ratings">
-    
-    <title>Game Database</title>
-
-    <!-- for multiple fonts change | to %7c * no spaces*  -->
-    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet">  
-
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/data_style.css">    <!-- custom style sheet -->
-
-    
-</head>
-
-<body>
-    
-    <p class="message">Eek!  Your browser does not support grid.  Please upgrade your system.</p>
-        
-    <div class="wrapper">
-    
-        <!-- logo / small image goes here -->
-        <div class="box logo">
-            <a href="index.php"><img src="images/logo.png" width="261" height="150" alt="Dice" /></a>
-        </div> <!-- / logo -->
-        
-        <div class="box banner">
-            <h1>Games Database</h1>        
-        </div> <!-- / banner -->
+<?php include("topbit.php") ?>
                        
             
         <div class="box main">
@@ -48,21 +13,4 @@
             
         </div> <!-- / main -->
         
-        <div class="box side">
-           
-           <h2>Search Area</h2>
-           
-           <p>This is where the search area goes
-
-            
-        </div> <!-- / side bar -->
-        
-        <div class="box footer">
-            CC Patrick Baker 2021
-        </div> <!-- / footer -->
-                
-        
-    </div> <!-- / wrapper -->
-    
-            
-</body>
+ <?php include("bottombit.php") ?>
